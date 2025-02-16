d0e54fff492f42c7f61209f7c580fd19da8cfd6c
arajab <azizbr@gmail.com>
2025-02-17 00:27:24 +0300

    Removed open projects and fixed layouts

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -94,7 +94,7 @@ const pendingProjectsList = [
   'make-your-own'
 ];
 
-const Dashboard: React.FC = () => {
+const Dashboard = () => {
   const router = useRouter();
   const [user, setUser] = useState<User>();
   const [moduleEvent, setModuleEvent] = useState<UserModuleEvent>();
@@ -308,7 +308,7 @@ const Dashboard: React.FC = () => {
       </Head>
       <Layout>
         <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-8">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-8">
             <CardDataStats
               title="Level"
               total={moduleEvent ? moduleEvent.level.toString() : ''}
@@ -375,32 +375,6 @@ const Dashboard: React.FC = () => {
                   d="M4,21a1,1,0,0,1-.71-.29,1,1,0,0,1,0-1.42l16-16a1,1,0,1,1,1.42,1.42l-16,16A1,1,0,0,1,4,21Z"
                   fill=""
                 />
-              </svg>
-            </CardDataStats>
-            <CardDataStats
-              title="Open Projects"
-              total={openProjects ? openProjects.size.toString() : '0'}
-              info={openProjects?.size ? Array.from(openProjects.keys()) : []}
-            >
-              <svg
-                className="fill-primary dark:fill-white"
-                width="800px"
-                height="800px"
-                viewBox="-5 -5 42 42"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g
-                  id="Icon-Set"
-                  transform="translate(-568.000000, -463.000000)"
-                  fill=""
-                >
-                  <path
-                    d="M597,481 L570,481 L570,467 C570,465.896 570.896,465 572,465 L595,465 C596.104,465 597,465.896 597,467 L597,481 L597,481 Z M597,485 C597,486.104 596.104,487 595,487 L572,487 C570.896,487 570,486.104 570,485 L570,483 L597,483 L597,485 L597,485 Z M582,489 L586,489 L586,493 L582,493 L582,489 Z M595,463 L572,463 C569.791,463 568,464.791 568,467 L568,485 C568,487.209 569.791,489 572,489 L580,489 L580,493 L578,493 C577.447,493 577,493.448 577,494 C577,494.553 577.447,495 578,495 L590,495 C590.553,495 591,494.553 591,494 C591,493.448 590.553,493 590,493 L588,493 L588,489 L595,489 C597.209,489 599,487.209 599,485 L599,467 C599,464.791 597.209,463 595,463 L595,463 Z"
-                    id="desktop"
-                    fill=""
-                  />
-                </g>
               </svg>
             </CardDataStats>
           </div>
