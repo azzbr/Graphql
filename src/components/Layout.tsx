50cee7bcbe20dc7a21e276d3de2715f02be7c470
arajab <azizbr@gmail.com>
2025-02-17 00:14:18 +0300

    Fixed Header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from 'react';
-import Header from './Header';
-import BreadCrumbs from './BreadCrumbs/BreadCrumbs';
 
 interface LayoutProps {
   children: ReactNode;
@@ -13,14 +11,9 @@ const Layout = ({ children }: LayoutProps) => {
       <div className="flex h-screen overflow-hidden">
         {/* <!-- ===== Content Area Start ===== --> */}
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Start ===== --> */}
-          <Header />
-          {/* <!-- ===== Header End ===== --> */}
-
           {/* <!-- ===== Main Content Start ===== --> */}
           <main>
             <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              <BreadCrumbs />
               {children}
             </div>
           </main>
